55f556d0202f24742d1df99f9bcf2faf416b509d
BarZu <1419328765@qq.com>
2017-01-19 11:42:52 +0800

    v0.0.4

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ module.exports = function (options) {
     if (styleText) {
       css = '\n[jtaro' + id + '] ' + styleText[1].replace(/\bthis\b/, '').trim()
         .replace(/}\s+(?!$)/g, '}\n[jtaro' + id + '] ')
-        .replace(/\([^\n\r]+\)/g, function (match) {
+        .replace(/:[^;}]+(;|\})/g, function (match) {
           return match.replace(/,/g, '<mark>')
         })
         .split(/,\s+/).join(',\n[jtaro' + id + '] ')
