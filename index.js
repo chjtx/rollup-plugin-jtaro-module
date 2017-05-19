850e0cf41c10d7078ebf67e9a3939479713f5b2b
BarZu <1419328765@qq.com>
2017-05-19 16:10:12 +0800

    v0.2.0

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var utils = require('rollup-pluginutils')
 var path = require('path')
+var sourceMap = require('source-map')
 
 module.exports = function (options) {
   options = options || {}
@@ -106,15 +107,70 @@ module.exports = function (options) {
       }
     },
     transformBundle: function (source) {
-      var style = []
-      var code = ''
-      // bundle后将预先标记的style代码前置到开头执行，确保样式优先
-      code = source.replace(/_____\(\"<<<<<([\s\S]+?)>>>>>\"\);?[\n\r]+?/g, function (match, css) {
-        style.push(css)
-        return ''
+      var generator = new sourceMap.SourceMapGenerator({
+        file: 'j'
       })
+      generator.setSourceContent("j", source)
+      
+      var line = 0
+      var column = 0
+      var original = [] // 代码改变前的行列
+      var style = []
+      // bundle后将预先标记的style代码前置到开头执行，确保样式优先
+      var code = source.replace(/_____\(\"<<<<<([\s\S]+?)>>>>>\"\);?[\n\r]+?|(\n)/g, function (match, css, enter, pos) {
+        line++
+        if (css) {
+          // 标记转换前的位置
+          original.push({
+            line: line,
+            content: css
+          })
+          style.push(css)
+          return ''
+        } else {
+          return match
+        }
+      })
+      // 查找转换后的行列
+      column = code.indexOf('###jtaro_module_style_text###')
+      var tempA = code.substr(0, column)
+      var tempB = tempA.split('\n')
+      column = column - (tempB.length ? tempB[tempB.length - 1].length : 0)
+
+      // 最终转换的代码
       code = code.replace('###jtaro_module_style_text###', style.join(''))
-      return code
+
+      // 添加变更过的mapping
+      original.forEach(function (ori) {
+        generator.addMapping({
+          source: 'j',
+          original: { line: ori.line, column: 1 },
+          generated: { line: tempB.length, column: code.indexOf(ori.content) - column + 1 }
+        })
+      })
+
+      // 添加没变更的代码的mapping
+      // 比较两文件的每一行，如果code的n行不等于source的n行且code的n行在source里，比较code的n行与sourcen+step行即可
+      var codeArr = code.split('\n')
+      var sourceArr = source.split('\n')
+      var step = 0
+      for (var i = 0, l = codeArr.length; i < l; i++) {
+        if (codeArr[i] === sourceArr[i + step]) {
+          generator.addMapping({
+            source: 'j',
+            original: { line: i + 1 + step, column: 1 },
+            generated: { line: i + 1, column: 1 }
+          })
+        } else if (source.indexOf(codeArr[i]) > -1) {
+          i--
+          step++
+        }
+      }
+
+      return {
+        code: code,
+        map: JSON.parse(generator.toString())
+      }
     }
   }
 }
